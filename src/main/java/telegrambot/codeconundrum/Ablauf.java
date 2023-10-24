59323bc9b3a58a2f9b7bf536c50bebca2ae6fbe9
VivianeNeubert <viviane.neubert@web.de>
2023-10-24 13:05:39 +0200

    Create classes

---
+++ src/main/java/telegrambot/codeconundrum/Ablauf.java
@@ -0,0 +1,31 @@
+package telegrambot.codeconundrum;
+
+public class Ablauf {
+
+    /*
+    - Lösung
+    - Hinweise
+    - Verdächtige
+    - Namen für den Detektiv
+    - Fall
+    - Highscore Zahl der benötigten Fragen
+
+    Quick, detective, there's been a murder!
+    We need your expertise!
+    What shall we call you?
+    Fall vorstellen
+    Optionen auch vorstellen: Orte, Personen
+    Alle Branches auf GitHub sind verschwunden! Wer hat auf die Phishing-Mail geklickt?? Wer hat sie verschickt?
+    - Person wechseln
+    - Person Frage stellen
+    - Orte besuchen (Text mit Beschreibung wird ausgegeben - Kantine, Serverraum, Schreibtische)
+    - Fall auflösen
+    - (Spiel neu starten, abbrechen)
+
+    Fall auflösen: Abgleich mit Täter, nach Keywords aus den Hinweisen suchen für Begründung (Motive, Means, Opportunity)
+    ansonsten: Täter ist entkommen oder Beweislast nicht ausreichend
+
+
+
+     */
+}

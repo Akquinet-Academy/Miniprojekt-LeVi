8a9d9327cceae9be73d1b4ff004d7b4b43ef88dc
VivianeNeubert <viviane.neubert@web.de>
2023-10-24 13:30:14 +0200

    Create places

--- src/main/java/telegrambot/codeconundrum/Case.java
+++ src/main/java/telegrambot/codeconundrum/Case.java
@@ -11,6 +11,18 @@ public class Case {
     }
 
     void createSuspects() {
-        Person larry = new Person("Larry der Lausige Lint", false);
+        Person larry = new Person("Larry Lint", false);
     }
+
+    void createPlaces() {
+        Place serverRoom = new Place("Server Room", "You are standing in the company's server room.");
+        Place cafeteria = new Place("Cafeteria", "You are standing in the company's cafeteria.");
+        Place larrysDesk = new Place("Larry's desk", "You are standing in front of Larry's desk.");
+        Place ritasDesk = new Place("Larry's desk", "You are standing in front of Rita's desk.");
+        Place samsDesk = new Place("Larry's desk", "You are standing in front of Sam's desk.");
+        Place maggiesDesk = new Place("Larry's desk", "You are standing in front of Maggie's desk.");
+        Place hubertsDesk = new Place("Larry's desk", "You are standing in front of Hubert's desk.");
+    }
+
+
 }
